db18738c1d0793a73d4414ab6fb58496234d1306
Preston Stone <stonepreston05@gmail.com>
2025-01-03 12:14:19 -0700

    Edited the vite config and uncommented all the required server code, started on the typedefs and resolvers

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,13 +9,11 @@ export default defineConfig({
     open: true,
     proxy: {
       // Uncomment the following code once you have built the queries and mutations in the client folder
-      // '/graphql': {
-      // Comment out the following code once you have built the queries and mutations in the client folder
-      '/api': {
-        target: 'http://localhost:3001',
+      '/graphql': {
+      target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
-      },
-    }
+    },
+  }
   }
 })
